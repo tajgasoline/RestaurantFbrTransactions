fcfd5e06b7caf44b0d6307999e8704fc2d31caad
Rabeet <rabeet.siddiqui22@gmail.com>
2023-01-04 16:38:10 +0500

    printer work updated

--- invoice.php
+++ invoice.php
@@ -5,11 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="printer.css">
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/icons.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/metisMenu.min.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+    
     <link href="assets/printer/printer.css" rel="stylesheet" type="text/css">
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
 
@@ -21,27 +17,27 @@
 
         <center><img src="assets/images/hoteltaj.png" alt="Logo" style="width: 45%; height: 45%;"></center>
  
-        <h6 style="text-align: center;">STNT NO:S113071-7
-        <h6 style="text-align: center;"><b><span id="staffusername">Salman</span></h6>
+        <h6 class="cust-cla" style="text-align: center;" >STNT NO:S113071-7
+        <h6 class="cust-cla" style="text-align: center;"><b><span id="staffusername">Salman</span></h6>
         <div class="">
-            <h6 class="mb-2" style="text-align: center; width: 100%; height: 10%;"><b>Address: </b> AUTOBHAN ROAD,
+            <h6 class="cust-cla" style="text-align: center; width: 100%; height: 10%;"><b>Address: </b> AUTOBHAN ROAD,
                 HYDERABAD
                 <b>BILL</b>
             </h6>
-            <h6 class="mb-2" style="text-align: center;"><b> Tarrif Heading: </b> 9801.2000
+            <h6 class="cust-cla" style="text-align: center;"><b> Tarrif Heading: </b> 9801.2000
                 <b>COPY</b>
             </h6>
             <div style="clear: both">
-                <h6 style="float: left"><b>KOT NO:</b></h6>
-                <h6 style="float: right"><b>INV No: </b><span id="transactionid">00000</span></h6>
+                <h6 style="text-align: center;" class="cust-cla"><b>KOT NO:</b></h6>
+                <h6 style="text-align: center;" class="cust-cla"><b>INV No: </b><span id="transactionid">00000</span></h6>
 
             </div>
             <div style="clear: both">
-                <h6 style="text-align: center;"><b>Table No: </b>RT-Fatima Hall 1.</h6>
-                <h6 style="text-align: center;"><b>Persons: </b><span id="PERSONS">5</span></h6>
+                <h6 style="text-align: center;" class="cust-cla"><b>Table No: </b>RT-Fatima Hall 1.</h6>
+                <h6 style="text-align: center;" class="cust-cla"><b>Persons: </b><span id="PERSONS">5</span></h6>
 
-                <h6 style="text-align: center;"><b>Order Type: </b> <span id="ORDERTYPE">Dine In</span></h6>
-                <h6 style="text-align: center;"><b>Date: </b><span id="STARTDATE">04/01/2023</span></h6>
+                <h6 style="text-align: center;" class="cust-cla"><b>Order Type: </b> <span id="ORDERTYPE">Dine In</span></h6>
+                <h6 style="text-align: center;" class="cust-cla"><b>Date: </b><span id="STARTDATE">04/01/2023</span></h6>
             </div>
         </div>
         </p>
@@ -94,10 +90,10 @@
                     <td></td>
                     <td>171,000</td>
                 </tr>
-                <tr  colspan="4">
+                <tr>
+                <td colspan="4" style="text-align: center;">Change Back</td>
                     <td></td>
                     <td></td>
-                    <td>Change Back</td>
                     <td></td>
 
                 </tr>
@@ -130,15 +126,8 @@
     <button id="btnPrint" class="hidden-print">Print</button>
     <script src="printer.js"></script>
 
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/metisMenu.min.js"></script>
-    <script src="assets/js/waves.min.js"></script>
-    <script src="assets/js/jquery.slimscroll.min.js"></script>
-    <script src="functions/invoice.js"></script>
 
-    <!-- App js -->
-    <script src="assets/js/app.js"></script>
+
     <script src="assets/printer/printer.js"></script>
     <script src="functions/Invoice.js"></script>
 </body>
