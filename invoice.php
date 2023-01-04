e20da966d48f274a5cb4119899b6fe5de98b91e3
Rabeet <rabeet.siddiqui22@gmail.com>
2023-01-04 15:47:58 +0500

    receipt made successfully

--- invoice.php
+++ invoice.php
@@ -1,141 +1,146 @@
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title>Hotel TAJ</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="A premium admin dashboard template by Mannatthemes" name="description">
-    <meta content="Mannatthemes" name="author">
-
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="assets/images/hoteltaj.png">
-
-    <!-- App css -->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="printer.css">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="assets/css/icons.css" rel="stylesheet" type="text/css">
     <link href="assets/css/metisMenu.min.css" rel="stylesheet" type="text/css">
     <link href="assets/css/style.css" rel="stylesheet" type="text/css">
-          <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+    <link href="assets/printer/printer.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
 
+    <title>Receipt example</title>
 </head>
 
 <body>
-    <div class="row">
+    <div class="ticket">
+
+        <center><img src="assets/images/hoteltaj.png" alt="Logo" style="width: 45%; height: 45%;"></center>
+ 
+        <h6 style="text-align: center;">STNT NO:S113071-7
+        <h6 style="text-align: center;"><b><span id="staffusername">Salman</span></h6>
+        <div class="">
+            <h6 class="mb-2" style="text-align: center; width: 100%; height: 10%;"><b>Address: </b> AUTOBHAN ROAD,
+                HYDERABAD
+                <b>BILL</b>
+            </h6>
+            <h6 class="mb-2" style="text-align: center;"><b> Tarrif Heading: </b> 9801.2000
+                <b>COPY</b>
+            </h6>
+            <div style="clear: both">
+                <h6 style="float: left"><b>KOT NO:</b></h6>
+                <h6 style="float: right"><b>INV No: </b><span id="transactionid">00000</span></h6>
+
+            </div>
+            <div style="clear: both">
+                <h6 style="text-align: center;"><b>Table No: </b>RT-Fatima Hall 1.</h6>
+                <h6 style="text-align: center;"><b>Persons: </b><span id="PERSONS">5</span></h6>
+
+                <h6 style="text-align: center;"><b>Order Type: </b> <span id="ORDERTYPE">Dine In</span></h6>
+                <h6 style="text-align: center;"><b>Date: </b><span id="STARTDATE">04/01/2023</span></h6>
+            </div>
+        </div>
+        </p>
+        <table>
+            <thead>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Qty</th>
+                    <th>Unit Price</th>
+                    <th>Total Price</th>
+
+                </tr>
+            </thead>
+            <tbody id="tbody">
+                <tr>
+                    <th>Family Festival-3 01</th>
+                    <td>1</td>
+                    <td>2,090.00</td>
+                    <td>2,090.00</td>
+
+                </tr>
+                <tr>
+                    <th>Subtotal</th>
+                    <td></td>
+                    <td></td>
+                    <td>1430</td>
+
+                    </tr>
+                    <tr>
+                    <th>SST13%</th>
+                    <td></td>
+                    <td></td>
+                    <td>19661</td>
+                    </tr>
+                    <tr>
+                    <th>Total Discount</th>
+                    <td></td>
+                    <td></td>
+                    <td>0.00</td>
+                    </tr>
+                    <tr>
+                    <th>Total</th>
+                    <td></td>
+                    <td></td>
+                    <td>170906</td>
+                    </tr>
+                    <tr>
+                    <th>CASH</th>
+                    <td></td>
+                    <td></td>
+                    <td>171,000</td>
+                </tr>
+                <tr  colspan="4">
+                    <td></td>
+                    <td></td>
+                    <td>Change Back</td>
+                    <td></td>
+
+                </tr>
+                <tr>
+                    <th>CASH</th>
+                    <td></td>
+                    <td></td>
+                    <td>93.00</td>
+
+                </tr>
+                <tr>
+                    <th></th>
+                    <td></td>
+                    <td></td>
+                    <td>0.00</td>
+                </tr>
+                
+</tr>
+            </tbody>
         
-        <div class="col-lg-5 mx-auto">
-            <div class="card">
-                <div class="card-body invoice-head">
-                      <div class="float-right d-print-none justify-content-right">
-                                    <a href="javascript:window.print()" class="btn btn-info"><i class="fa fa-print"></i></a>
-                                  
-                                </div>
-                    <div class="row">
-                        <div class="col-md-12" style="text-align: center;">
+        </table>
+        
+      
+        
+        </table>
+        <p class="centered">Thank You For Coming
+            <br>See You Again
+        </p>
+    </div>
+    <button id="btnPrint" class="hidden-print">Print</button>
+    <script src="printer.js"></script>
 
-                            <img src="assets/images/hoteltaj.png" alt="logo-large" class="logo-lg" height="200"
-                            style="text-align: center;">
-                        </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/metisMenu.min.js"></script>
+    <script src="assets/js/waves.min.js"></script>
+    <script src="assets/js/jquery.slimscroll.min.js"></script>
+    <script src="functions/invoice.js"></script>
 
-                    </div>
-                    <div style="clear: both">
-                        <h6 style="float: left"><b>STNT NO: </b>S113071-7</h6>
-                        <h6 style="float: right"><b><span id="staffusername">-</span></b></h6>
-
-                    </div>
-
-
-                </div><!--end card-body-->
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="">
-                                <h6 class="mb-2" style="text-align: center;"><b>Address: </b> AUTOBHAN ROAD, HYDERABAD
-                                    <b>BILL</b> </h6>
-                                    <h6 class="mb-2" style="text-align: center;"><b> Tarrif Heading: </b> 9801.2000
-                                        <b>COPY</b></h6>
-                                        <div style="clear: both">
-                                            <h6 style="float: left"><b>KOT NO</b></h6>
-                                            <h6 style="float: right"><b>INV No: </b><span id="transactionid">00000</span></h6>
-
-                                        </div>
-                                        <div style="clear: both">
-                                            <h6 style="text-align: center;"><b>Table No </b>RT-Fatima Hall 1.</h6>
-                                            <h6 style="text-align: center;"><b>Persons </b><span id="PERSONS">-</span></h6>
-                                            
-                                            <h6 style="float: left"><b>Order Type </b> <span id="ORDERTYPE">-</span></h6>
-                                            <h6 style="float: right"><b>Date: </b><span id="STARTDATE">-</span></h6>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <br>
-
-
-
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="table-responsive">
-                                        <table class="table table-bordered mb-0">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>Product Name</th>
-                                                            <th>Qty</th>
-                                                            <th>Unit Price</th>                                                    
-                                                            <th>Total Price</th>
-                                                            
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody id="tbody">
-                                                        <tr>
-                                                            <th>Family Festival-3 01</th>
-                                                            <td>1</td>
-                                                            <td>2,090.00</td>
-                                                            <td>2,090.00</td>
-                                                            
-                                                        </tr>  
-                                                       
-                                                    </tbody>
-                                                </table>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row justify-content-center">
-                                <div class="col-lg-6">
-                                    <h5 class="mt-4">THANKS FOR COMING</h5>
-                                    <h5 class="mt-4">SEE YOU AGAIN</h5>
-                                </div>
-
-                            </div>
-
-                          
-                        </div>
-                    </div>
-                </div><!--end card-->
-            </div><!--end col-->
-        </div><!--end row-->
-
-    </div><!-- container -->
-
-
-</div>
-<!-- end page content -->
-</div>
-<!-- end page-wrapper -->
-
-<!-- jQuery  -->
-<script src="../assets/js/jquery.min.js"></script>
-<script src="../assets/js/bootstrap.bundle.min.js"></script>
-<script src="../assets/js/metisMenu.min.js"></script>
-<script src="../assets/js/waves.min.js"></script>
-<script src="../assets/js/jquery.slimscroll.min.js"></script>
-<script src="functions/Invoice.js"></script>
-
-<!-- App js -->
-<script src="../assets/js/app.js"></script>
-
-
+    <!-- App js -->
+    <script src="assets/js/app.js"></script>
+    <script src="assets/printer/printer.js"></script>
+    <script src="functions/Invoice.js"></script>
 </body>
 
 </html>
---
+++ invoice3.php
@@ -0,0 +1,141 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Hotel TAJ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A premium admin dashboard template by Mannatthemes" name="description">
+    <meta content="Mannatthemes" name="author">
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/hoteltaj.png">
+
+    <!-- App css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/icons.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/metisMenu.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+          <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+
+</head>
+
+<body>
+    <div class="row">
+        
+        <div class="col-lg-5 mx-auto">
+            <div class="card">
+                <div class="card-body invoice-head">
+                      <div class="float-right d-print-none justify-content-right">
+                                    <a href="javascript:window.print()" class="btn btn-info"><i class="fa fa-print"></i></a>
+                                  
+                                </div>
+                    <div class="row">
+                        <div class="col-md-12" style="text-align: center;">
+
+                            <img src="assets/images/hoteltaj.png" alt="logo-large" class="logo-lg" height="200"
+                            style="text-align: center;">
+                        </div>
+
+                    </div>
+                    <div style="clear: both">
+                        <h6 style="float: left"><b>STNT NO: </b>S113071-7</h6>
+                        <h6 style="float: right"><b><span id="staffusername">-</span></b></h6>
+
+                    </div>
+
+
+                </div><!--end card-body-->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="">
+                                <h6 class="mb-2" style="text-align: center;"><b>Address: </b> AUTOBHAN ROAD, HYDERABAD
+                                    <b>BILL</b> </h6>
+                                    <h6 class="mb-2" style="text-align: center;"><b> Tarrif Heading: </b> 9801.2000
+                                        <b>COPY</b></h6>
+                                        <div style="clear: both">
+                                            <h6 style="float: left"><b>KOT NO</b></h6>
+                                            <h6 style="float: right"><b>INV No: </b><span id="transactionid">00000</span></h6>
+
+                                        </div>
+                                        <div style="clear: both">
+                                            <h6 style="text-align: center;"><b>Table No </b>RT-Fatima Hall 1.</h6>
+                                            <h6 style="text-align: center;"><b>Persons </b><span id="PERSONS">-</span></h6>
+                                            
+                                            <h6 style="float: left"><b>Order Type </b> <span id="ORDERTYPE">-</span></h6>
+                                            <h6 style="float: right"><b>Date: </b><span id="STARTDATE">-</span></h6>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <br>
+
+
+
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered mb-0">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Product Name</th>
+                                                            <th>Qty</th>
+                                                            <th>Unit Price</th>                                                    
+                                                            <th>Total Price</th>
+                                                            
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody id="tbody">
+                                                        <tr>
+                                                            <th>Family Festival-3 01</th>
+                                                            <td>1</td>
+                                                            <td>2,090.00</td>
+                                                            <td>2,090.00</td>
+                                                            
+                                                        </tr>  
+                                                       
+                                                    </tbody>
+                                                </table>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row justify-content-center">
+                                <div class="col-lg-6">
+                                    <h5 class="mt-4">THANKS FOR COMING</h5>
+                                    <h5 class="mt-4">SEE YOU AGAIN</h5>
+                                </div>
+
+                            </div>
+
+                          
+                        </div>
+                    </div>
+                </div><!--end card-->
+            </div><!--end col-->
+        </div><!--end row-->
+
+    </div><!-- container -->
+
+
+</div>
+<!-- end page content -->
+</div>
+<!-- end page-wrapper -->
+
+<!-- jQuery  -->
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/metisMenu.min.js"></script>
+<script src="../assets/js/waves.min.js"></script>
+<script src="../assets/js/jquery.slimscroll.min.js"></script>
+<script src="functions/Invoice.js"></script>
+
+<!-- App js -->
+<script src="../assets/js/app.js"></script>
+
+
+</body>
+
+</html>
